4d01664796fa05be3f5e428fb82793454db5a423
Luis <luiseduardosa20006@gmail.com>
2024-10-23 12:46:42 -0500

    CARAY

--- ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
+++ ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
@@ -16,25 +16,33 @@ public class Ejemplo05 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Divide pero solo como entero
         System.out.println("----------------");
         System.out.println(10 / 3);
         
+        // divide pero como float
         System.out.println("----------------");
         System.out.println(10 / 3.0);
         
+        // resuelve por gerarquia de operaciones
         System.out.println("----------------");
         System.out.println(10 / 3.0 + 1);
         System.out.println(10 / (3.0 + 1));
         
         System.out.println("----------------");
+        // esta operación es para hacer el calculo del residuo
         System.out.println(10 % 3 );
         
+        // esta operación es para hacer una potencia
         System.out.println("----------------");
         System.out.println(Math.pow(10, 2));
         
+        // esta operación es para la raiz cuadrada
         System.out.println("----------------");
         System.out.println(Math.sqrt(16));
         
+        // sirve para almacenar informacion en una variable
         double m = Math.sqrt(16);
         System.out.println(m);
         
